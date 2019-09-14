ddfce5d866af8683d1a578c4a16aca03293ec73d
Chris Ruettimann <chris@bitbull.ch>
2019-09-14 17:58:31 +0200

    debugging

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -e
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
-IMAGE=$(cat IMAGE)
-IMAGE_FROM=$(cat IMAGE_FROM)
-VERSION=$(cat VERSION)
 
-cd $(dirname $0) 
-sed -i "s@^FROM .*@FROM $IMAGE_FROM:$VERSION@" Dockerfile
-
-docker build -t $IMAGE:$VERSION .
+docker build -t christian773/xfce-vnc:latest .
 
