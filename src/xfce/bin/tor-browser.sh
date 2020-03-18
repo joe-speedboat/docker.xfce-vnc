fbd84ceecc91b9e128c1d88cb86238bd9d9c4213
Chris Ruettimann <chris@bitbull.ch>
2020-03-18 11:26:39 +0100

    automated build

---
+++ src/xfce/bin/tor-browser.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ADIR=$HOME/.local/share/applications
+PDIR=$HOME/bin
+TURL="https://dist.torproject.org$(curl -s https://www.torproject.org/download/ | grep linux | grep download | sed 's/.*href=..dist//' | cut -d\" -f1 )"
+TFILE="tor-browser_en-US.tar.xz"
+
+cd
+test -d $PDIR || mkdir $PDIR
+cd $PDIR
+test -d tor-browser*
+if [ $? -ne 0 ]
+then
+   curl -s $TURL -o $TFILE
+   tar xf $TFILE
+   rm -f $TFILE
+
+   test -d $ADIR || mkdir -p $ADIR
+   mkdir -p $ADIR
+   ICON="$ADIR/tor-browser.desktop"
+   echo '[Desktop Entry]' > $ICON
+   echo 'Type=Application' >> $ICON
+   echo 'Name=Tor Browser Setup' >> $ICON
+   echo 'Categories=Network;WebBrowser;Security;' >> $ICON
+   echo "Exec=$HOME/bin/tor-browser.sh" >> $ICON
+   echo 'Icon=web-browser' >> $ICON
+fi
+exec $HOME/bin/tor-browser_en-US/Browser/start-tor-browser --detach
