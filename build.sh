f9c4d063e36a66915a5e90fed6d2971a1f0b7889
Chris Ruettimann <chris@bitbull.ch>
2020-03-22 12:37:27 +0100

    release var added bugfix in background

--- build.sh
+++ build.sh
@@ -3,16 +3,23 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
 REG=docker.io/christian773/xfce-vnc
 REG_PATT=$(echo $REG | cut -d/ -f2-)
 VERSION=$(cat VERSION)
+RELEASE=latest
+if [ '$1' == '-s' ]
+then
+   RELEASE=stable
+fi
+
+echo "INFO: Building Release: $RELEASE   /   VERSION: $VERSION"
 
 test -f Dockerfile || (echo "ERROR: Dockerfile not found" ; exit 1)
 sed -i "s/^ENV REFRESHED_AT.*/ENV REFRESHED_AT $(date '+%Y-%m-%d-%H:%M')/" Dockerfile
 sed -i "s/^ENV VERSION.*/ENV VERSION $VERSION/" Dockerfile
 
 git add -A
-git commit -a -m "automated build"
+git commit -a -m "automated build, RELEASE: $RELEASE"
 
 docker system prune -a -f
-docker build -t $REG:latest -t $REG:$VERSION --build-arg CACHEBUST=$(date +%s) .
-docker push $REG:latest
+docker build -t $REG:$RELEASE -t $REG:$VERSION --build-arg CACHEBUST=$(date +%s) .
+docker push $REG:$RELEASE
 docker push $REG:$VERSION
 
