c75f9661c0fb31ac711a1204ce49e5414a0bfeba
Christian Ruettimann <chris@bitbull.ch>
2021-11-18 09:22:10 +0100

    automated build, RELEASE: latest

--- build.sh
+++ build.sh
@@ -16,16 +16,16 @@ sed -i "s/^ENV VERSION.*/ENV VERSION $VERSION/" Dockerfile
 git add -A
 git commit -a -m "automated build, RELEASE: $RELEASE"
 
-docker system prune -a -f
-docker build -t $REG:$VERSION .
-docker tag $REG:$VERSION $REG:$RELEASE
-docker push $REG:$RELEASE
-docker push $REG:$VERSION
+sudo docker system prune -a -f
+sudo docker build -t $REG:$VERSION .
+sudo docker tag $REG:$VERSION $REG:$RELEASE
+sudo docker push $REG:$RELEASE
+sudo docker push $REG:$VERSION
 
 echo "Done,
 If you want to push this release with stable tag as well,
 do your testing now and hit <ENTER>.
 If not, type <CTRL>+<C>"
 read x
-docker tag $REG:$VERSION $REG:stable
-docker push $REG:stable
+sudo docker tag $REG:$VERSION $REG:stable
+sudo docker push $REG:stable
